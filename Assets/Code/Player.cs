058d296ac399706092a93db395a948c874fc0af8
Jeffreeyy <17844@ma-web.nl>
2015-09-14 10:12:41 +0200

    ayy
    lmao

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    private int m_score = 0;
+	private float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //beweeg de speler naar rechts
+        this.transform.position += Vector3.right * speed * Time.deltaTime;
+
+		switch(m_score)
+		{
+		case 1:
+			break;
+		case 2:
+			speed = .5f;
+			break;
+		case 3:
+			this.transform.Rotate(0,0,1);
+			break;
+		}
+    }
+
+	public void coinsCollected(){
+		m_score ++;
+		Debug.Log (m_score);
+	}
+}
